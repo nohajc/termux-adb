2b8591c54865f3365f4e53baf4cf3d661c58acf1
Jan Noha <nohajc@gmail.com>
2022-10-05 01:10:13 +0200

    we really need to hook open but id does not work

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 extern crate lazy_static;
 
 use std::{
-    ffi::{CString, CStr},
+    ffi::{CString, CStr, VaList},
     fs::File,
     io::Write,
     os::raw::{c_char, c_int},
@@ -63,42 +63,30 @@ hook! {
 }
 
 hook! {
-    unsafe fn __open_2(pathname: *const c_char, flags: c_int) -> c_int => tadb_open {
-        let fn_ptr: *const c_void = std::mem::transmute(&__open_2);
-        log!("[TADB] called open at {:?}", fn_ptr);
+    unsafe fn __open_2(pathname: *const c_char, flags: c_int) -> c_int => tadb_open_2 {
         let real_open = real!(__open_2);
-        let real_fn_ptr: *const c_void = std::mem::transmute(real_open);
-        log!("[TADB] real open at {:?}", real_fn_ptr);
-
         let name = to_string(CStr::from_ptr(pathname));
 
-        log!("[TADB] called open with name={} flags={}", name, flags);
+        log!("[TADB] called __open_2 with name={} flags={}", name, flags);
         let result = real_open(pathname, flags);
 
-        log!("[TADB] open returned fd with value {}", result);
+        log!("[TADB] __open_2 returned fd with value {}", result);
         result
     }
 }
 
 // #[no_mangle]
-// pub unsafe extern "C" fn open(pathname: *const c_char, flags: c_int, mut args: ...) -> c_int {
-//     let fn_ptr: *const c_void = std::mem::transmute(&open);
-//     log!("[TADB] called open at {:?}", fn_ptr);
+// pub unsafe extern fn open(pathname: *const c_char, flags: c_int) -> c_int {
+//     // let fn_ptr: *const c_void = std::mem::transmute(&open);
+//     // log!("[TADB] called open at {:?}", fn_ptr);
 //     let real_open = real2!(open);
-//     let real_fn_ptr: *const c_void = std::mem::transmute(real_open);
-//     log!("[TADB] real open at {:?}", real_fn_ptr);
+//     // let real_fn_ptr: *const c_void = std::mem::transmute(real_open);
+//     // log!("[TADB] real open at {:?}", real_fn_ptr);
 
 //     let name = to_string(CStr::from_ptr(pathname));
 
-//     let result = if (flags & O_CREAT) == 0 {
-//         log!("[TADB] called open with name={} flags={}", name, flags);
-//         real_open(pathname, flags)
-//     }
-//     else {
-//         let mode = args.arg::<mode_t>();
-//         log!("[TADB] called open with name={} flags={} mode={}", name, flags, mode);
-//         real_open(pathname, flags, mode)
-//     };
+//     log!("[TADB] called open with name={} flags={}", name, flags);
+//     let result = real_open(pathname, flags);
 
 //     log!("[TADB] open returned fd with value {}", result);
 //     result
