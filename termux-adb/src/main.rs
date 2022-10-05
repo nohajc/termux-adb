109027cbbb9c3c500efa55928d58a6f2c64080e5
nohajc <nohajc@gmail.com>
2022-10-06 01:28:24 +0200

    termux-adb: start adb server and wait for it to terminate

---
+++ termux-adb/src/main.rs
@@ -0,0 +1,37 @@
+use std::{process::{Command, ExitCode, ExitStatus}, ffi::OsStr, io, time::Duration};
+use nix::{unistd, sys::signal};
+use sysinfo::{SystemExt, RefreshKind, ProcessRefreshKind, ProcessExt, Pid};
+
+fn exec_adb<I: IntoIterator<Item = S>, S: AsRef<OsStr>>(args: I) -> io::Result<ExitStatus> {
+    Command::new("adb").args(args).status()
+}
+
+fn wait_for(pid: Pid) {
+    let pid = unistd::Pid::from_raw(i32::from(pid));
+    while let Ok(()) = signal::kill(pid, None) {
+        std::thread::sleep(Duration::from_secs(1))
+    };
+}
+
+fn main() -> ExitCode {
+    if let Err(e) = exec_adb(["kill-server"]) {
+        eprintln!("{}", e);
+        return ExitCode::FAILURE;
+    }
+
+    if let Err(e) = exec_adb(["start-server"]) {
+        eprintln!("{}", e);
+        return ExitCode::FAILURE;
+    }
+
+    let system = sysinfo::System::new_with_specifics(
+        RefreshKind::new()
+            .with_processes(ProcessRefreshKind::new())
+    );
+
+    if let Some(p) = system.processes_by_exact_name("adb").next() {
+        wait_for(p.pid());
+    }
+
+    ExitCode::SUCCESS
+}
