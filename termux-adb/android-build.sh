cf28224f2fe5465817db4c8692a00eb61d44d96c
Jan Noha <nohajc@gmail.com>
2022-10-06 21:30:47 +0200

    added build and deploy scripts for termux-adb wrapper

---
+++ termux-adb/android-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# arm
+export PATH=$PATH:$HOME/Android/android-ndk-r22b/toolchains/llvm/prebuilt/windows-x86_64/bin
+#export CC=aarch64-linux-android28-clang
+#export CXX=aarch64-linux-android28-clang++
+
+cargo build --release --target aarch64-linux-android
