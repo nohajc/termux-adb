5e57ef1014bc83893d1c9cff81db278cdbcc47af
nohajc <nohajc@gmail.com>
2022-10-07 23:57:12 +0200

    prepared in-memory directory structure

--- adb-hooks/src/lib.rs
+++ adb-hooks/src/lib.rs
@@ -5,14 +5,19 @@ extern crate lazy_static;
 
 use std::{
     env,
-    ffi::{CString, CStr},
+    ffi::{CString, CStr, OsStr},
     fs::File,
     io::Write,
+    mem,
     os::raw::{c_char, c_int},
-    sync::Mutex,
+    sync::Mutex, collections::HashMap, path::PathBuf,
 };
 
-use libc::{DIR, dirent, O_CREAT, mode_t};
+use std::os::unix::ffi::OsStrExt;
+
+use libc::{DIR, dirent, O_CREAT, mode_t, DT_CHR, DT_DIR};
+
+use rand::Rng;
 
 use redhook::{
     hook, real, real2,
@@ -34,6 +39,7 @@ macro_rules! log {
 }
 
 fn to_string(s: &CStr) -> String {
+    // OsStr::from_bytes(s.to_bytes()).to_owned()
     match s.to_str() {
         Ok(s) => s.to_owned(),
         Err(e) => e.to_string(),
@@ -44,6 +50,66 @@ fn to_cstr(b: &[c_char]) -> &CStr {
     unsafe { CStr::from_ptr(b.as_ptr()) }
 }
 
+// our directory structure will always be flat
+// so we can have just one dirent per DirStream
+struct DirStream {
+    pos: i32,
+    entry: dirent,
+}
+
+trait NameSetter {
+    fn set_name(&mut self, name: &OsStr);
+}
+
+impl NameSetter for dirent {
+    fn set_name(&mut self, name: &OsStr) {
+        for (i, j) in self.d_name.iter_mut().zip(
+            name.as_bytes().iter().chain([0].iter())
+        ) {
+            *i = *j as i8;
+        }
+    }
+}
+
+fn dirent_new(off: i64, typ: u8, name: &OsStr) -> dirent {
+    let mut rng = rand::thread_rng();
+    let mut entry = dirent {
+        d_ino: rng.gen(),
+        d_off: off,
+        d_reclen: mem::size_of::<dirent>() as u16,
+        d_type: typ,
+        d_name: [0; 256],
+    };
+    entry.set_name(name);
+
+    entry
+}
+
+lazy_static! {
+    static ref DIR_MAP: Mutex<HashMap<PathBuf, DirStream>> = Mutex::new({
+        let mut dir_map = HashMap::new();
+        if let Ok(usb_dev_path) = env::var("TERMUX_USB_DEV").map(|str| PathBuf::from(str)) {
+            if let Some(usb_dev_name) = usb_dev_path.file_name() {
+                let mut last_entry = dirent_new(
+                    0, DT_CHR, usb_dev_name
+                );
+                let mut current_dir = usb_dev_path;
+
+                while current_dir.pop() {
+                    dir_map.insert(current_dir.clone(), DirStream{
+                        pos: 0,
+                        entry: last_entry
+                    });
+                    last_entry = dirent_new(
+                        0, DT_DIR, current_dir.as_os_str()
+                    )
+                }
+            }
+        }
+        dir_map
+    });
+}
+
 hook! {
     unsafe fn opendir(name: *const c_char) -> *mut DIR => tadb_opendir {
         let dir_name = to_string(CStr::from_ptr(name));
@@ -51,6 +117,12 @@ hook! {
         let remapped_name_c = CString::new(remapped_name.as_str()).unwrap();
         log!("[TADB] called opendir with {}, remapping to {}", &dir_name, &remapped_name);
         real!(opendir)(remapped_name_c.as_ptr())
+    }
+}
+
+hook! {
+    unsafe fn closedir(dirp: *mut DIR) -> c_int => tadb_closedir {
+        real!(closedir)(dirp)
     }
 }
 
@@ -85,7 +157,7 @@ type OpenFn = unsafe extern "C" fn(*const c_char, c_int, ...) -> c_int;
 pub unsafe extern "C" fn open(pathname: *const c_char, flags: c_int, mut args: ...) -> c_int {
     // let real_open = real2!(open);
     // There is some problem with caching the real function value (TODO: fix)
-    let real_open: OpenFn = std::mem::transmute(redhook::ld_preload::dlsym_next("open\0"));
+    let real_open: OpenFn = mem::transmute(redhook::ld_preload::dlsym_next("open\0"));
 
     let name = to_string(CStr::from_ptr(pathname));
     // eprintln!("DEBUG name: {}", name);
