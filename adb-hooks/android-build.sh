05f2cf2da6d61d2d866bc2b398095f00933ed091
nohajc <nohajc@gmail.com>
2022-10-10 21:08:37 +0200

    build scripts cleanup

--- adb-hooks/android-build.sh
+++ adb-hooks/android-build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# arm
-export PATH=$PATH:$HOME/Android/android-ndk-r22b/toolchains/llvm/prebuilt/windows-x86_64/bin
-#export CC=aarch64-linux-android28-clang
-#export CXX=aarch64-linux-android28-clang++
-
+cargo build --release --target armv7-linux-androideabi
 cargo build --release --target aarch64-linux-android
-#cargo build --release --target aarch64-unknown-linux-gnu
