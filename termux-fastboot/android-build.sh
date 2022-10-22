8c6a6768d6b0a3acb65ebf0f5e0f0f923fcab9ae
Jan Noha <nohajc@gmail.com>
2022-10-22 18:54:02 +0200

    added termux-fastboot launcher

---
+++ termux-fastboot/android-build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cargo build --release --target armv7-linux-androideabi
+cargo build --release --target aarch64-linux-android
---
+++ termux-fastboot/android-deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SSH_HOST=u0_a342@192.168.1.156
+SSH_PORT=8022
+
+#scp -P $SSH_PORT tadb.sh $SSH_HOST:~/termux-adb
+scp -P $SSH_PORT target/aarch64-linux-android/release/termux-fastboot $SSH_HOST:~/termux-adb
