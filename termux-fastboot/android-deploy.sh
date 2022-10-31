c6a3be377550809be0107ca13f0c30259b485d84
Jan Noha <nohajc@gmail.com>
2022-10-31 22:29:50 +0100

    improved deploy scripts

--- termux-fastboot/android-deploy.sh
+++ termux-fastboot/android-deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-SSH_HOST=u0_a342@192.168.1.156
-SSH_PORT=8022
-
-#scp -P $SSH_PORT tadb.sh $SSH_HOST:~/termux-adb
-scp -P $SSH_PORT target/aarch64-linux-android/release/termux-fastboot $SSH_HOST:~/termux-adb
+source $(dirname -- "${BASH_SOURCE[0]}")/../deploy-util.sh
+scp target/$TARGET_ARCH_TRIPLE/release/termux-fastboot $SSH_TARGET:~/termux-adb
