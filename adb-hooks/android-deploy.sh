78a96f210557773f31859571cf87f1b26b147074
Jan Noha <nohajc@gmail.com>
2022-11-05 12:51:11 +0100

    added more shell script helpers, fixed script path resolution

--- adb-hooks/android-deploy.sh
+++ adb-hooks/android-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-source $(dirname -- "${BASH_SOURCE[0]}")/../deploy-util.sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source "$SCRIPT_DIR/../deploy-util.sh"
 scp target/$TARGET_ARCH_TRIPLE/release/libadbhooks.so $SSH_TARGET:~/termux-adb
---
+++ make-release.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+   echo "usage: $0 VERSION"
+   exit 1
+fi
+
+VERSION=$1
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo "SCRIPT_DIR=$SCRIPT_DIR"
+
+cd "$SCRIPT_DIR"
+
+# Build
+(cd adb-hooks && ./android-build.sh)
+(cd termux-adb && ./android-build.sh)
+(cd termux-fastboot && ./android-build.sh)
+
+# Archive
+mkdir -p dist
+cd dist
+
+mkdir "$VERSION"
+cd "$VERSION"
+
+function archive_release {
+   RELEASE_DIR=$1
+   TARGET_TRIPLE=$2
+
+   mkdir "$RELEASE_DIR"
+   cd "$RELEASE_DIR"
+   cp "$SCRIPT_DIR/termux-adb/target/$TARGET_TRIPLE/release/termux-adb" .
+   cp "$SCRIPT_DIR/adb-hooks/target/$TARGET_TRIPLE/release/libadbhooks.so" .
+   cp "$SCRIPT_DIR/termux-fastboot/target/$TARGET_TRIPLE/release/termux-fastboot" .
+   cd ..
+   tar cvpzf "$RELEASE_DIR.tar.gz" "$RELEASE_DIR"
+   rm -r "$RELEASE_DIR"
+}
+
+archive_release "termux-adb-$VERSION-aarch64" "aarch64-linux-android"
+archive_release "termux-adb-$VERSION-armv7" "armv7-linux-androideabi"
