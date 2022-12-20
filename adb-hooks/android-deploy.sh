6fa830e152fb82d449aa03ff528cff517734839a
nohajc <nohajc@gmail.com>
2022-12-20 16:16:01 +0100

    cleanup

--- adb-hooks/android-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source "$SCRIPT_DIR/../deploy-util.sh"
-scp target/$TARGET_ARCH_TRIPLE/release/libadbhooks.so $SSH_TARGET:~/termux-adb
--- termux-fastboot/android-build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cargo build --release --target armv7-linux-androideabi
-cargo build --release --target aarch64-linux-android
