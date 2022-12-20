0737b5e9ac26811cdd4eaa1657d0e29f5de95fd7
nohajc <nohajc@gmail.com>
2022-12-20 15:28:31 +0100

    removed trailing whitespace

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ apt-get  --assume-yes upgrade
 apt-get  --assume-yes install coreutils gnupg wget
 if [ ! -f "$PREFIX/etc/apt/sources.list.d/termux-adb.list" ]; then
   mkdir -p $PREFIX/etc/apt/sources.list.d
-  echo -e "deb https://nohajc.github.io termux extras" > $PREFIX/etc/apt/sources.list.d/termux-adb.list 
-  wget -qP $PREFIX/etc/apt/trusted.gpg.d https://nohajc.github.io/nohajc.gpg 
+  echo -e "deb https://nohajc.github.io termux extras" > $PREFIX/etc/apt/sources.list.d/termux-adb.list
+  wget -qP $PREFIX/etc/apt/trusted.gpg.d https://nohajc.github.io/nohajc.gpg
   apt update
   apt install termux-adb
-else 
+else
   echo "Repo already installed"
   apt install termux-adb
 fi
